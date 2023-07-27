eaf2314e21e25074862aff1d34dfccc4e42da6e9
Igor Nagrebelnyi <nagrebelnyi.igor@gmail.com>
2023-07-28 00:43:17 +0300

    task-2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,14 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const ingredientsRef = document.querySelector('ul#ingredients');
+
+const markup = ingredients.map(ingredient => {
+  const element = document.createElement('li');
+  element.textContent = ingredient;
+  element.classList.add('item');
+  return element;
+});
+
+console.log(markup);
+
+ingredientsRef.append(...markup);
