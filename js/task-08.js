6e1c9f6cb5a9ac7dab2fbeff62bf95af30f1cddd
Igor Nagrebelnyi <nagrebelnyi.igor@gmail.com>
2023-07-31 23:37:11 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,36 @@
+const formRef = document.querySelector('.login-form');
 
+formRef.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+
+  //   const { email, password } = evt.currentTarget.elements;
+
+  //   if (!email.value || !password.value) {
+  //     alert('Всі поля повинні бути заповнені');
+  //     return;
+  //   }
+
+  //   const formData = {
+  //     email: email.value,
+  //     password: password.value,
+  //   };
+
+  const formElements = evt.currentTarget.elements;
+  const email = formElements.email.value;
+  const password = formElements.password.value;
+
+  if (!email || !password) {
+    alert('Всі поля повинні бути заповнені!');
+    return;
+  }
+
+  const formData = {
+    email,
+    password,
+  };
+
+  console.log(formData);
+  formRef.reset();
+}
