ec7639c1a150b9c0d52853c245790c1595ed6e26
Igor Nagrebelnyi <nagrebelnyi.igor@gmail.com>
2023-08-01 00:16:00 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,17 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const refs = {
+  colorName: document.querySelector('.color'),
+  button: document.querySelector('.change-color'),
+  body: document.body,
+};
+
+refs.button.addEventListener('click', onButtonClick);
+
+function onButtonClick() {
+  const bodyColor = getRandomHexColor();
+  refs.colorName.textContent = bodyColor;
+  refs.body.style.backgroundColor = bodyColor;
+}
